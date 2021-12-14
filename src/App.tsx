d5b6d716481fabfc71d46b3bafd0674ab1af780e
Gulshan <shrigulshankumar@gmail.com>
2021-12-14 23:53:18 +0200

    Added props array

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Greetings from './components/Greetings';
 
 function App() {
+  const familyList = [
+    {
+      first: 'John',
+      last: 'Pekka'
+    },
+    {
+      first: 'Matt',
+      last: 'Juho'
+    }
+  ]
   return (
     <div className="App">
  
-    <Greetings name='Gulshan' today='20.10.2021' />
+    <Greetings name='Gulshan' today='20.10.2021' family={familyList} />
     </div>
   );
 }
--- src/components/Greetings.tsx
+++ src/components/Greetings.tsx
@@ -1,12 +1,21 @@
 type greetingProps = {
     name: string,
     today: string|number
+    family: {
+        first: string,
+        last: string
+    }[]
 }
 export const Greetings = (props: greetingProps) => {
     return (  
         <div>
                <h1>Welcome {props.name}!</h1> 
                <h1>{props.today}</h1>
+               { props.family.map(perhe => {
+                   return (
+                       <h1>{perhe.first} - {perhe.last} </h1>
+                   )
+               } )}
 
         </div>
     );
